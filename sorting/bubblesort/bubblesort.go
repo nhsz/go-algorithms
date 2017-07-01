6223ead4a65a7153b688fdb8ca1d8b9b8f6b70d8
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-07-01 13:59:29 -0300

    Add complexity comments

--- sorting/bubblesort/bubblesort.go
+++ sorting/bubblesort/bubblesort.go
@@ -4,26 +4,32 @@ package bubblesort
 // Complexity: O(n^2)
 func Sort(list []int) {
 	n := len(list)
-	swapCounter := -1
+	numberOfSwaps := -1
 
-	for swapCounter != 0 {
-		swapCounter = 0
+	// If numberOfSwaps == 0 in a pass, then the slice is already sorted
+	for numberOfSwaps != 0 {
+		numberOfSwaps = 0
+		// Complexity: O(n^2) = n iterations * Complexity(pass) = n * n
 		for i := 0; i < n; i++ {
-			pass(list, i, swapCounter)
+			pass(list, i, numberOfSwaps)
 		}
 	}
 }
 
-func pass(list []int, previousPasses, swapCounter int) {
+// Complexity: O(n)
+func pass(list []int, previousPasses, numberOfSwaps int) {
 	n := len(list)
+	// In the i-th iteration, the element at the n - i position of the slice
+	// is placed correctly, so you don't have to check it
 	for i := 0; i+1 < n-previousPasses; i++ {
 		if list[i] > list[i+1] {
 			swap(list, i)
-			swapCounter++
+			numberOfSwaps++
 		}
 	}
 }
 
+// Complexity: O(1)
 func swap(list []int, i int) {
 	list[i], list[i+1] = list[i+1], list[i]
 }
