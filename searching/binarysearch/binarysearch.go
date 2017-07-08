8937b2c228833c40e20969f0c4e5d5b7b78bffcb
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-07-08 19:29:00 -0300

    Remove else block

--- searching/binarysearch/binarysearch.go
+++ searching/binarysearch/binarysearch.go
@@ -6,14 +6,14 @@ package binarysearch
 func Search(list []int, value, min, max int) int {
 	if max < min {
 		return -1
-	} else {
-		centralPosition := (min + max) / 2
-
-		if value < list[centralPosition] {
-			return Search(list, value, min, centralPosition-1)
-		} else if value > list[centralPosition] {
-			return Search(list, value, centralPosition+1, max)
-		}
-		return centralPosition
 	}
+
+	centralPosition := (min + max) / 2
+
+	if value < list[centralPosition] {
+		return Search(list, value, min, centralPosition-1)
+	} else if value > list[centralPosition] {
+		return Search(list, value, centralPosition+1, max)
+	}
+	return centralPosition
 }
