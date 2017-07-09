74d2b9f5ba61219d714d28a21a70ab3d40a8fc9f
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-07-08 23:58:27 -0300

    Add iterative version of Binary Search

--- searching/binarysearch/binarysearch.go
+++ searching/binarysearch/binarysearch.go
@@ -1,19 +1,41 @@
 package binarysearch
 
-// Search implements the Binary Search algorithm with a slice of integers
+// RecursiveSearch implements a recursive version of the Binary Search
+// algorithm with a slice of integers
 // Prerequisite: the list is already sorted
 // Complexity: O(log(n))
-func Search(list []int, value, min, max int) int {
+func RecursiveSearch(list []int, value, min, max int) int {
 	if max < min {
 		return -1
 	}
 
-	centralPosition := (min + max) / 2
+	mid := (min + max) / 2
 
-	if value < list[centralPosition] {
-		return Search(list, value, min, centralPosition-1)
-	} else if value > list[centralPosition] {
-		return Search(list, value, centralPosition+1, max)
+	if value < list[mid] {
+		return RecursiveSearch(list, value, min, mid-1)
+	} else if value > list[mid] {
+		return RecursiveSearch(list, value, mid+1, max)
 	}
-	return centralPosition
+	return mid
+}
+
+// IterativeSearch implements an iterative version of the Binary Search
+// algorithm with a slice of integers
+// Prerequisite: the list is already sorted
+// Complexity: O(log(n))
+func IterativeSearch(list []int, value int) int {
+	min := 0
+	max := len(list) - 1
+
+	for min <= max {
+		mid := (min + max) / 2
+		if value == list[mid] {
+			return mid
+		} else if value < list[mid] {
+			max = mid - 1
+		} else {
+			min = mid + 1
+		}
+	}
+	return -1
 }
--- searching/binarysearch/binarysearch_test.go
+++ searching/binarysearch/binarysearch_test.go
@@ -6,48 +6,92 @@ import (
 
 // Input: [], 3
 // Expected: -1
-func TestBinarySearch1(t *testing.T) {
+func TestBinaryRecursiveSearch1(t *testing.T) {
 	list := []int{}
 	n := len(list) - 1
 	value := 3
 
-	if position := Search(list, value, 0, n); position != -1 {
+	if position := RecursiveSearch(list, value, 0, n); position != -1 {
 		t.Errorf("Result was incorrect, got: %v, want: -1.", position)
 	}
 }
 
 // Input: [1], 3
 // Expected: -1
-func TestBinarySearch2(t *testing.T) {
+func TestBinaryRecursiveSearch2(t *testing.T) {
 	list := []int{1}
 	n := len(list) - 1
 	value := 3
 
-	if position := Search(list, value, 0, n); position != -1 {
+	if position := RecursiveSearch(list, value, 0, n); position != -1 {
 		t.Errorf("Result was incorrect, got: %v, want: -1.", position)
 	}
 }
 
 // Input: [1, 3], 3
 // Expected: -1
-func TestBinarySearch3(t *testing.T) {
+func TestBinaryRecursiveSearch3(t *testing.T) {
 	list := []int{1, 3}
 	n := len(list) - 1
 	value := 3
 
-	if position := Search(list, value, 0, n); position != 1 {
+	if position := RecursiveSearch(list, value, 0, n); position != 1 {
 		t.Errorf("Result was incorrect, got: %v, want: 1.", position)
 	}
 }
 
 // Input: [0, 1, 2, 3, 4, 5, 7], 4
 // Expected: 4
-func TestBinarySearch4(t *testing.T) {
+func TestBinaryRecursiveSearch4(t *testing.T) {
 	list := []int{0, 1, 2, 3, 4, 5, 7}
 	n := len(list) - 1
 	value := 4
 
-	if position := Search(list, value, 0, n); position != 4 {
+	if position := RecursiveSearch(list, value, 0, n); position != 4 {
+		t.Errorf("Result was incorrect, got: %v, want: 4.", position)
+	}
+}
+
+// Input: [], 3
+// Expected: -1
+func TestBinaryIterativeSearch1(t *testing.T) {
+	list := []int{}
+	value := 3
+
+	if position := IterativeSearch(list, value); position != -1 {
+		t.Errorf("Result was incorrect, got: %v, want: -1.", position)
+	}
+}
+
+// Input: [1], 3
+// Expected: -1
+func TestBinaryIterativeSearch2(t *testing.T) {
+	list := []int{1}
+	value := 3
+
+	if position := IterativeSearch(list, value); position != -1 {
+		t.Errorf("Result was incorrect, got: %v, want: -1.", position)
+	}
+}
+
+// Input: [1, 3], 3
+// Expected: -1
+func TestBinaryIterativeSearch3(t *testing.T) {
+	list := []int{1, 3}
+	value := 3
+
+	if position := IterativeSearch(list, value); position != 1 {
+		t.Errorf("Result was incorrect, got: %v, want: 1.", position)
+	}
+}
+
+// Input: [0, 1, 2, 3, 4, 5, 7], 4
+// Expected: 4
+func TestBinaryIterativeSearch4(t *testing.T) {
+	list := []int{0, 1, 2, 3, 4, 5, 7}
+	value := 4
+
+	if position := IterativeSearch(list, value); position != 4 {
 		t.Errorf("Result was incorrect, got: %v, want: 4.", position)
 	}
 }
